5d411669b88ff882a86a85a1aab64b9634975cc3
mincomk <mail@drchi.co.kr>
2025-05-28 11:56:29 +0900

    fix: lua

--- src/OS.lua
+++ src/OS.lua
@@ -1,5 +1,5 @@
 return {
-   version = (function() return function() return os.version() end end)
-   getComputerId = (function() return function() return os.getComputerId() end end)
-   getComputerLabel = (function() return function() return os.getComputerLabel() end end)
+   version = (function() return function() return os.version() end end),
+   getComputerId = (function() return function() return os.getComputerId() end end),
+   getComputerLabel = (function() return function() return os.getComputerLabel() end end),
 }
